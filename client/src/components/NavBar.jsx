e777475f6271aa92f03447e7ef49b5e03972eed1
Maria <mariaisa1207@gmail.com>
2022-06-24 19:11:24 -0500

    add navbar and sidebar components

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.scss'
+
+export default function NavBar() {
+  return (
+    <div  className="">
+      <nav className='navbar navbar-expand-sm navbar-dark - bg-dark'>
+        <h1 className='p-3 navbar-brand'>Historial de gastos personales</h1>
+        
+      </nav>
+  </div>
+  )
+}
---
+++ client/src/components/SideBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import {FaHome}  from 'react-icons/fa';
+import {BiCategory} from 'react-icons/bi';
+import {CgDetailsMore} from 'react-icons/cg';
+import '../App.scss';
+
+
+export default function SideBar() {
+  return (
+    <div className='sidebar bg-dark'>
+          <ul >
+            <li><NavLink activeClassName='active' className='text-white rounded py-2 w-100 d-inline-block px-3'
+            to='/' ><FaHome className='me-2'/> Home </NavLink></li>
+            <li><NavLink activeClassName='active' className='text-white rounded py-2 w-100 d-inline-block px-3'
+            to='category' > <BiCategory className='me-2'/>Category</NavLink></li>
+            <li><NavLink activeClassName='active' className='text-white rounded py-2 w-100 d-inline-block px-3'
+            to='/all' ><CgDetailsMore className='me-2'/> All</NavLink></li>
+          </ul>
+        </div>
+   
+  )
+}
+
